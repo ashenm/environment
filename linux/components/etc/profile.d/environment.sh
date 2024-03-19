2f14ad44d59f579e786645e3db946854fd989f76
Ashen Gunaratne <mail@ashenm.ml>
2024-03-19 18:19:10 +0000

    [linux] Update PS1

--- linux/components/etc/profile.d/environment.sh
+++ linux/components/etc/profile.d/environment.sh
@@ -57,3 +57,24 @@ then
   alias vim='vim -p'
 
 fi
+
+case "$TERM" in
+  xterm-color|*-256color)
+      color_prompt='\[\033[;32m\]'
+      color_info='\[\033[1;34m\]'
+      color_reset='\[\033[0m\]'
+      color_regular_bold='\[\033[0;1m\]'
+      prompt_symbol='@'
+      ;;
+  *)
+      color_prompt=''
+      color_info=''
+      color_reset='\[\033[0m\]'
+      color_regular_bold=''
+      prompt_symbol='@'
+      ;;
+esac
+
+PS1=$color_prompt'┌──${debian_chroot:+($debian_chroot)──}${VIRTUAL_ENV:+('$color_regular_bold'$(basename $VIRTUAL_ENV)'$color_prompt')}('$color_info'\u'$prompt_symbol'\h'$color_prompt')-['$color_regular_bold'\w'$color_prompt']\n'$color_prompt'└─'$color_info'\$'$color_reset' '
+
+unset color_prompt color_info color_reset prompt_symbol
